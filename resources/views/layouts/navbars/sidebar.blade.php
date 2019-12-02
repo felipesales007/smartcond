91f850ba01863305bb58ab16858ee92c7b3d19af
Felipe Sales <felipesales007@hotmail.com>
2019-12-02 00:46:47 -0300

    ajuste do btn do sidebar

--- resources/views/layouts/navbars/sidebar.blade.php
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -12,9 +12,9 @@
             <div class="ml-auto mt-1">
                 <div class="sidenav-toggler d-none d-xl-block" data-action="sidenav-unpin" data-target="#sidenav-main">
                     <div class="sidenav-toggler-inner">
-                        <i class="sidenav-toggler-line"></i>
-                        <i class="sidenav-toggler-line"></i>
-                        <i class="sidenav-toggler-line"></i>
+                        <i class="sidenav-toggler-line bg-primary"></i>
+                        <i class="sidenav-toggler-line bg-primary"></i>
+                        <i class="sidenav-toggler-line bg-primary"></i>
                     </div>
                 </div>
             </div>
